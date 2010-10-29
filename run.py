dc3b80851ede6d61e7022de9c6afcc20cd29a7d3
ALTER <alter.pub@gmail.com>
2010-10-29 16:30:09 +0400

    README - describe howto and depending TODO - add what to do Daemon.py - add daemon to plugin run.ru - rewrote the code with using Daemon.py chmod -x *.py && chmod +x run.py

--- run.py
+++ run.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-#import daemon
+from Daemon import Daemon 
 from log import Log 
 from parser import Parser
 from mysql import MySQL
+import sys,time
 
-def run():
-	log = Log()
-	parser = Parser()
-	mysql = MySQL()
-	log.Open("./wp-uniqip.log")
-#	log.Write("wp-uniqip is starting")
-	log.Write(parser.Open("/var/log/nginx/localhost.access_log"))
-	log.Write(mysql.Connect("localhost", "root", " ", "wordpress"))
-	log.Write(mysql.Insert(parser.GetUniqIP()))
-	log.Write(mysql.Disconnect())
-	log.Write(parser.Close())
-	log.Close()
+class UniqIP(Daemon):
 
-#with daemon.DaemonContext():
-run()
+	def run(self):
+		while True:
+			log = Log()
+			parser = Parser()
+			mysql = MySQL()
+			log.Open("/home/alter/git/wp-uniqip/log.log") 					# log path
+			log.Write(parser.Open("/var/log/nginx/localhost.access_log")) 	# access log path (apache,nginx,lighttpd, etc...)
+			log.Write(mysql.Connect("localhost", "root", " ", "wordpress"))	# hostname, username, password, database for mysql
+			log.Write(mysql.Insert(parser.GetUniqIP()))
+			log.Write(mysql.Disconnect())
+			log.Write(parser.Close())
+			log.Close()
+			time.sleep(60*15) 												# 15 minutes
+
+
+if __name__ == "__main__":
+	daemon = UniqIP('/tmp/wp-uniqip.pid')									# pid path(better to change path to /var/run/wp-uniqip.pid)
+	if (len(sys.argv) == 2):
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		else:
+			print "Unknown command\n USE start|restart|stop\n"
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print "usage: %s start|restart|stop" % sys.argv[0]
+		sys.exit(2)
 

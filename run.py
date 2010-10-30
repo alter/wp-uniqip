f070f1739210bbf3d0ad0727b89fe126bdfe9b1b
alterpub <alter.pub@gmail.com>
2010-10-30 17:18:04 +0400

    Release 1.0 is ready

--- run.py
+++ run.py
@@ -5,39 +5,61 @@ from Daemon import Daemon
 from log import Log 
 from parser import Parser
 from mysql import MySQL
+from optparse import OptionParser
 import sys,time
 
 class UniqIP(Daemon):
+	logpath			=	"/tmp/wp-uniqip.log"					# log path
+	accesslogpath	=	"/var/log/nginx/localhost.access_log"	# access log path (apache,nginx,lighttpd, etc...)
+	mysql_hostname	=	"localhost"								# mysql host name
+	mysql_username	=	"root"									# mysql user name
+	mysql_password	=	" "										# mysql user password
+	mysql_database	=	"wordpress"								# mysql database name
+
+	log = Log()
+	parser = Parser()
+	mysql = MySQL()
 
 	def run(self):
 		while True:
-			log = Log()
-			parser = Parser()
-			mysql = MySQL()
-			log.Open("/home/alter/git/wp-uniqip/log.log") 					# log path
-			log.Write(parser.Open("/var/log/nginx/localhost.access_log")) 	# access log path (apache,nginx,lighttpd, etc...)
-			log.Write(mysql.Connect("localhost", "root", " ", "wordpress"))	# hostname, username, password, database for mysql
-			log.Write(mysql.Insert(parser.GetUniqIP()))
-			log.Write(mysql.Disconnect())
-			log.Write(parser.Close())
-			log.Close()
-			time.sleep(60*15) 												# 15 minutes
-
+			self.log.Open(self.logpath)
+			self.log.Write(self.parser.Open(self.accesslogpath))
+			self.log.Write(self.mysql.Connect(self.mysql_hostname, self.mysql_username, self.mysql_password, self.mysql_database))
+			self.log.Write(self.mysql.Insert(self.parser.GetUniqIP()))
+			self.log.Write(self.mysql.Disconnect())
+			self.log.Write(self.parser.Close())
+			self.log.Close()
+			time.sleep(60*15) 								# 15 minutes
+	
+	def install(self):
+		print self.mysql.Connect(self.mysql_hostname, self.mysql_username, self.mysql_password, self.mysql_database)
+		self.mysql.DropTable()
+		self.mysql.DropTrigger()
+		self.mysql.CreateTable()
+		self.mysql.CreateTrigger()
+		print self.mysql.Disconnect()
 
 if __name__ == "__main__":
-	daemon = UniqIP('/tmp/wp-uniqip.pid')									# pid path(better to change path to /var/run/wp-uniqip.pid)
-	if (len(sys.argv) == 2):
-		if 'start' == sys.argv[1]:
-			daemon.start()
-		elif 'restart' == sys.argv[1]:
-			daemon.restart()
-		elif 'stop' == sys.argv[1]:
-			daemon.stop()
-		else:
-			print "Unknown command\n USE start|restart|stop\n"
-			sys.exit(2)
-		sys.exit(0)
+	daemon = UniqIP('/tmp/wp-uniqip.pid')                   # pid path(better to change path to /var/run/wp-uniqip.pid)
+	usage = "\t./%prog [options]"
+	parser = OptionParser(usage=usage)
+	parser.add_option("-i", "--install", action="store_true", dest="install", default="False", help="install table and trigger to db")
+	options,args = parser.parse_args()
+	if (options.install == True):
+		daemon.install()
 	else:
-		print "usage: %s start|restart|stop" % sys.argv[0]
-		sys.exit(2)
+		if (len(sys.argv) == 2):
+			if 'start' == sys.argv[1]:
+				daemon.start()
+			elif 'restart' == sys.argv[1]:
+				daemon.restart()
+			elif 'stop' == sys.argv[1]:
+				daemon.stop()
+			else:
+				print "Unknown command\n USE start|restart|stop\n"
+				sys.exit(2)
+			sys.exit(0)
+		else:
+			print "Usage: %s start|restart|stop" % sys.argv[0]
+			sys.exit(2)
 
